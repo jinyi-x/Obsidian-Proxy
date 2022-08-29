ab6c63cbc9fef51989794c7143cd6a06ef226ce8
jinyi-x <jinyi.12@qq.com>
2022-08-30 00:18:02 +0800

    Setting Tab

--- main.ts
+++ main.ts
@@ -1,137 +1,148 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { text } from 'stream/consumers';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+enum ProxyType_e {
+    none = 0,
+    http = 1,
+    socks5 = 2
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+interface ObsidianProxySettings {
+    ProxyType: ProxyType_e;
+    ProxyAddress: string;
+    ProxyPort: number;
+    ProxyUsername: string;
+    ProxyPassword: string;
+    EnableProxy: boolean;
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+const DEFAULT_SETTINGS: ObsidianProxySettings = {
+    ProxyType: ProxyType_e.none,
+    ProxyAddress: '',
+    ProxyPort: 0,
+    ProxyUsername: '',
+    ProxyPassword: '',
+    EnableProxy: false
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
+class ObsidianProxySettingTab extends PluginSettingTab {
+    constructor(app: App, plugin: ObsidianProxyPlugin) {
+        console.log('Setting Tab Loaded')
+        super(app, plugin)
+        this.plugin = plugin
+    }
+    plugin: ObsidianProxyPlugin
+    display(): void {
+        const {containerEl} = this
+        containerEl.empty();
+        containerEl.createEl('h2', {text: 'Obsidian Proxy Setup'});
+        new Setting(containerEl)
+            .setName('Proxy Type')
+            .setDesc('Select protocol of proxy server')
+            .addDropdown(dropDown => {
+                dropDown.addOption('Type', 'None');
+                dropDown.addOption('None', 'None');
+                dropDown.addOption('HTTP', 'HTTP');
+                dropDown.addOption('Socks5', 'Socks5');
+                dropDown.onChange(async (value) => {
+                    switch(value) {
+                        case 'HTTP':
+                            this.plugin.settings.ProxyType = ProxyType_e.http;
+                            break;
+                        case 'Socks5':
+                            this.plugin.settings.ProxyType = ProxyType_e.socks5;
+                            break;
+                        case 'None':
+                            this.plugin.settings.ProxyType = ProxyType_e.none;
+                            break;
+                        default:
+                            this.plugin.settings.ProxyType = ProxyType_e.none;
+                            break;
+                    }
+                    await this.plugin.saveSettings()
+                })
+            });
+        new Setting(containerEl)
+            .setName('Proxy Address')
+            .setDesc('Address of proxy server')
+            .addText(text => text
+                .setPlaceholder('Proxy Address')
+                .setValue(this.plugin.settings.ProxyAddress)
+                .onChange(async(value) => {
+                    this.plugin.settings.ProxyAddress = value;
+                    await this.plugin.saveSettings();
+                })
+            );
+        new Setting(containerEl)
+            .setName('Proxy Port')
+            .setDesc('Port of proxy server')
+            .addText(text => text
+                .setPlaceholder('Proxy Port')
+                .setValue(String(this.plugin.settings.ProxyPort))
+                .onChange(async(value) => {
+                    if(isNaN(Number(value))) {
+                        console.log('Invalid Value');
+                    } else {
+                        this.plugin.settings.ProxyPort = Number(value);
+                    }
+                    await this.plugin.saveSettings();
+                })
+            );
+        new Setting(containerEl)
+            .setName('Proxy Username')
+            .setDesc('Username of proxy server account')
+            .addText(text => text
+                .setPlaceholder('Proxy Username')
+                .setValue(this.plugin.settings.ProxyUsername)
+                .onChange(async(value)=>{
+                    this.plugin.settings.ProxyUsername = value;
+                    await this.plugin.saveSettings();
+                })
+            );
+        new Setting(containerEl)
+            .setName('Proxy Password')
+            .setDesc('Password of proxy server account')
+            .addText(text => text
+                .setPlaceholder('Proxy Password')
+                .setValue(this.plugin.settings.ProxyPassword)
+                .onChange(async(value) => {
+                    this.plugin.settings.ProxyPassword = value;
+                    await this.plugin.saveSettings();
+                })
+            );
+        new Setting(containerEl)
+            .setName('Enable')
+            .addDropdown(dropDown => {
+                dropDown.addOption('Enable', 'Enable');
+                dropDown.addOption('Disable', 'Disable');
+                dropDown.onChange(async(value) => {
+                    switch(value) {
+                        case 'Enable':
+                            this.plugin.settings.EnableProxy = true;
+                            break;
+                        case 'Disable':
+                            this.plugin.settings.EnableProxy = false;
+                            break;
+                        default:
+                            break
+                    }
+                    await this.plugin.saveSettings();
+                });
+            })
+    }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+export default class ObsidianProxyPlugin extends Plugin {
+    settings: ObsidianProxySettings
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
+    }
+    async saveSettings() {
+        await this.saveData(this.settings)
+    }
+    onunload(): void {}
+    async onload() {
+        await this.loadSettings()
+        this.addSettingTab(new ObsidianProxySettingTab(this.app, this));
+    }
 }
